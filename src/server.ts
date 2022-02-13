67b4407da4df4ed50896e69025c6a4dff8326d3c
valdir-ti <valdir.ti@gmail.com>
2022-02-13 14:57:44 -0300

    config eslint, prettier and ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'API rentalx' })
+})
+
+app.listen(3333, () => console.log('API running!'))
