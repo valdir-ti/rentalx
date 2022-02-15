cc3c03a52657e9541dd24969c6537983f97f4ea0
Valdir Silva <valdir.ti@gmail.com>
2022-02-15 17:55:38 -0300

    add typeorm

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import swaggerUi from "swagger-ui-express";
 import { routes } from "./routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const PORT = 3333;
 const app = express();
 
