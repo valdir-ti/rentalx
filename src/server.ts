0b0cd0a5f7d611523d05faede30a1fe23e0c3005
Valdir Silva <valdir.ti@gmail.com>
2022-02-15 10:26:17 -0300

    configure the swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { routes } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const PORT = 3333;
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/", (request, response) => {
     return response.json({ message: "API rentalx is running!" });
