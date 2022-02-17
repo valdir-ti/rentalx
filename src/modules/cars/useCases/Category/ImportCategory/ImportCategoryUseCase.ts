baebc4e936330319070f05c3d762132fd9189783
Valdir Silva <valdir.ti@gmail.com>
2022-02-17 00:08:54 -0300

    feat: import categories

--- src/modules/cars/useCases/Category/ImportCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/Category/ImportCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import { parse } from "csv-parse";
 import fs from "fs";
+import { inject, injectable } from "tsyringe";
 
 import { ICategoriesRepository } from "../../../repositories/Categories/ICategoriesRepository";
 
@@ -7,8 +8,12 @@ interface IImportCategory {
     name: string;
     description: string;
 }
+@injectable()
 class ImportCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {}
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: ICategoriesRepository
+    ) {}
 
     loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
         return new Promise((resolve, reject) => {
@@ -43,10 +48,12 @@ class ImportCategoryUseCase {
         categories.map(async (category) => {
             const { name, description } = category;
 
-            const categoryExists = this.categoriesRepository.findByName(name);
+            const categoryExists = await this.categoriesRepository.findByName(
+                name
+            );
 
             if (!categoryExists) {
-                this.categoriesRepository.create({ name, description });
+                await this.categoriesRepository.create({ name, description });
             }
         });
     }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/Category/CreateCategory/CreateCategoryController";
-import { importCategoryController } from "../modules/cars/useCases/Category/ImportCategory";
+import { ImportCategoryController } from "../modules/cars/useCases/Category/ImportCategory/ImportCategoryController";
 import { ListCategoriesController } from "../modules/cars/useCases/Category/ListCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
@@ -17,8 +17,11 @@ categoriesRoutes.get("/", listCategoriesController.handle);
 const createCategoryController = new CreateCategoryController();
 categoriesRoutes.post("/", createCategoryController.handle);
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    return importCategoryController.handle(request, response);
-});
+const importCategoryController = new ImportCategoryController();
+categoriesRoutes.post(
+    "/import",
+    upload.single("file"),
+    importCategoryController.handle
+);
 
 export { categoriesRoutes };
