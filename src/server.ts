2fefd54b79247539ae552f7dda81872ca8f460a3
Valdir Silva <valdir.ti@gmail.com>
2022-02-18 00:25:34 -0300

    feat: create logs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import "reflect-metadata";
+import bodyParser from "body-parser";
 import express from "express";
+import fs from "fs";
+import moment from "moment";
+import morganBody from "morgan-body";
+import path from "path";
 import swaggerUi from "swagger-ui-express";
 
 import { routes } from "./routes";
@@ -10,9 +15,28 @@ import "./database";
 import "./shared/container";
 
 const PORT = 3333;
+
 const app = express();
 
 app.use(express.json());
+
+app.use(bodyParser.json());
+
+const log = fs.createWriteStream(
+    path.join(
+        __dirname,
+        "./logs",
+        `express-${moment().tz("America/Sao_Paulo").format("YYYY-MM-DD")}.log`
+    ),
+    {
+        flags: "a",
+    }
+);
+
+morganBody(app, {
+    noColors: true,
+    stream: log,
+});
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
